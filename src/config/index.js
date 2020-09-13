8f72fdbe9be2b5734836504f6128e3784026715e
André Galdino <andre.galdinolima@gmail.com>
2020-09-13 00:29:28 -0300

    chore: add mysql database

--- src/config/index.js
+++ src/config/index.js
@@ -6,4 +6,12 @@ module.exports = {
   server: {
     port: process.env.PORT || 80,
   },
+  database: {
+    driver: process.env.DATABASE_DRIVER || 'mysql',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: process.env.DATABASE_PORT || '3306',
+    db: process.env.DATABASE_DB || 'marketplace',
+    username: process.env.DATABASE_USER || 'quero',
+    password: process.env.DATABASE_PASSWORD || 'secret',
+  },
 }
