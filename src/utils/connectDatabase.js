14d834c532f07e2b37c5fc05212fa54f90234a35
André Galdino <andre.galdinolima@gmail.com>
2020-09-13 02:00:02 -0300

    chore: add database connection

---
+++ src/utils/connectDatabase.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { Sequelize } = require('sequelize')
+
+const { database } = require('../config')
+const { db, host, username, password, port, driver } = database
+
+const sequelize = new Sequelize(db, username, password, {
+  host,
+  port,
+  dialect: driver,
+})
+
+module.exports = sequelize
